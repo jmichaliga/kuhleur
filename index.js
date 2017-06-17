6f474dc724f85527c8f04e37050f0e4a56a88c45
Justin Michaliga <justin.michaliga@gmail.com>
2017-06-17 11:57:13 -0400

    added server logging

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Good = require('good');
 const _ = require('lodash');
 const low = require('lowdb');
 
@@ -13,137 +14,115 @@ server.connection({
 });
 
 server.route({
-    method: 'POST',
-    path: '/applications',
-    handler: function(req, res){
-      let h = req.headers;
-      let c = h.college;
-      let n = h.name;
-      let s = h.score;
+  method: 'POST',
+  path: '/applications',
+  handler: function(req, res){
+    let h = req.headers;
+    let c = h.college;
+    let n = h.name;
+    let s = h.score;
 
-      let dupe = db.get('applications')
-        .find({ college: c, name: n })
-        .size()
-        .value()
+    let dupe = db.get('applications')
+      .find({ college: c, name: n })
+      .size()
+      .value()
 
-      let pass = (c && n && s && dupe == 0) ? true : false; //valid format w/out duplicate;
-      let data = {};
-      let payload = {college: c, name: n, score: s};
+    let pass = (c && n && s && dupe == 0) ? true : false; //valid format w/out duplicate;
+    let data = {};
+    let payload = {college: c, name: n, score: s};
 
-      if(pass){
-        data = {
-          statusCode: 200,
-          message: "Application submitted successfully"
-        }
-        db.get('applications')
-          .push(payload)
-          .last()
-          .write()
-      }else{
-        data = {
-          statusCode: 400,
-          error: "Bad request",
-          message:"Application already submitted for this college/name pair"
-        }
+    if(pass){
+      data = {
+        statusCode: 200,
+        message: "Application submitted successfully"
       }
-
-      res(data);
+      db.get('applications')
+        .push(payload)
+        .last()
+        .write()
+    }else{
+      data = {
+        statusCode: 400,
+        error: "Bad request",
+        message:"Application already submitted for this college/name pair"
+      }
     }
+
+    res(data);
+  }
 });
 
 server.route({
-    method: 'GET',
-    path: '/applicants',
-    handler: function(req, res){
+  method: 'GET',
+  path: '/applicants',
+  handler: function(req, res){
 
-      // let applicants = db.get('applications')
-      //   .map('name')
-      //   .cloneDeep()
-      //   .value();
+    let applicants = _.clone(db.get('applications')
+      .groupBy('name')
+      .value());
 
-      let applicants = _.clone(db.get('applications')
-        .groupBy('name')
-        .value());
-
-      var apps = _.forEach(applicants, function(a){
-        _.forEach(a, function(app){
-          delete app.name
-        });
-      });
-
-      res(apps);
-    }
+    res(applicants);
+  }
 });
 
 server.route({
-    method: 'GET',
-    path: '/applicants/{name}',
-    handler: function(req, res){
+  method: 'GET',
+  path: '/applicants/{name}',
+  handler: function(req, res){
 
-      let name = encodeURIComponent(req.params.name);
-      let applications = _.clone(db.get('applications')
-        .filter({'name': name})
-        .value());
+    let name = encodeURIComponent(req.params.name);
+    let applications = _.clone(db.get('applications')
+      .filter({'name': name})
+      .value());
 
-      let apps = _.forEach(applications, function(a){delete a.name});
+    let data = {
+      name: name,
+      applications: applications
+    };
 
-      let data = {
-        name: name,
-        applications: apps
-      };
-
-      res(data);
-    }
+    res(data);
+  }
 });
 
 server.route({
-    method: 'GET',
-    path: '/colleges',
-    handler: function(req, res){
+  method: 'GET',
+  path: '/colleges',
+  handler: function(req, res){
 
-      let colleges = _.clone(db.get('applications')
-        .groupBy('college')
-        .value());
+    let colleges = _.clone(db.get('applications')
+      .groupBy('college')
+      .value());
 
-      var apps = _.forEach(colleges, function(c){
-        _.forEach(c, function(app){
-          delete app.college
-        });
-      });
-
-      res(apps);
-    }
+    res(colleges);
+  }
 });
 
 server.route({
-    method: 'GET',
-    path: '/colleges/{name}',
-    handler: function(req, res){
+  method: 'GET',
+  path: '/colleges/{name}',
+  handler: function(req, res){
 
-      let name = encodeURIComponent(req.params.name);
-      let applications = _.clone(db.get('applications')
-        .filter({'college': name})
-        .value());
+    let name = encodeURIComponent(req.params.name);
+    let applications = _.clone(db.get('applications')
+      .filter({'college': name})
+      .value());
 
-      let apps = _.forEach(applications, function(a){delete a.college});
-      //let apps = _.forEach(applications, function(a){_.omit(a, 'college')});
+    let data = {
+      college: name,
+      applications: applications
+    };
 
-      let data = {
-        college: name,
-        applications: apps
-      };
-
-      res(data);
-    }
+    res(data);
+  }
 });
 
 server.route({
-    method: 'POST',
-    path: '/backup',
-    handler: function(req, res){
-      db.write('backup.json');
-      res({statusCode: 200, message:'Backup successful'});
-    }
+  method: 'POST',
+  path: '/backup',
+  handler: function(req, res){
+    db.write('backup.json');
+    res({statusCode: 200, message:'Backup successful'});
+  }
 });
 
 db.defaults({
@@ -162,15 +141,37 @@ db.defaults({
         "college": "Business",
         "name": "Bob",
         "score": 90
-    },
+    }
   ]
-})
-  .write()
+}).write();
 
-server.start((err) => {
+server.register({
+    register: Good,
+    options: {
+        reporters: {
+            console: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{
+                    response: '*',
+                    log: '*'
+                }]
+            }, {
+                module: 'good-console'
+            }, 'stdout']
+        }
+    }
+}, (err) => {
 
     if (err) {
-        throw err;
+        throw err; // something bad happened loading the plugin
     }
-    console.log(`Server running at: ${server.info.uri}`);
+
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
 });
