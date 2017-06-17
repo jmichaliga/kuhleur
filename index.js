ccf799000cdd994e641a377149dc54c43dee9194
Justin Michaliga <justin.michaliga@gmail.com>
2017-06-17 12:22:52 -0400

    rm fp methods

--- index.js
+++ index.js
@@ -146,32 +146,32 @@ db.defaults({
 }).write();
 
 server.register({
-    register: Good,
-    options: {
-        reporters: {
-            console: [{
-                module: 'good-squeeze',
-                name: 'Squeeze',
-                args: [{
-                    response: '*',
-                    log: '*'
-                }]
-            }, {
-                module: 'good-console'
-            }, 'stdout']
-        }
+  register: Good,
+  options: {
+    reporters: {
+      console: [{
+        module: 'good-squeeze',
+        name: 'Squeeze',
+        args: [{
+          response: '*',
+          log: '*'
+        }]
+      }, {
+        module: 'good-console'
+      }, 'stdout']
     }
+  }
 }, (err) => {
 
+  if (err) {
+    throw err; // something bad happened loading the plugin
+  }
+
+  server.start((err) => {
+
     if (err) {
-        throw err; // something bad happened loading the plugin
+        throw err;
     }
-
-    server.start((err) => {
-
-        if (err) {
-            throw err;
-        }
-        server.log('info', 'Server running at: ' + server.info.uri);
-    });
+    server.log('info', 'Server running at: ' + server.info.uri);
+  });
 });
